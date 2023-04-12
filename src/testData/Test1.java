556a3682fb9aca0535f56d59608570d114b5e7e4
Arunodaya1234 <foundationarunodaya@gmail.com>
2023-04-12 21:05:18 +0530

    Test1.java updated

--- src/testData/Test1.java
+++ src/testData/Test1.java
@@ -4,7 +4,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Test1 java file created");
-
+		System.out.println("Test1 java file created");
+		System.out.println("Test1 java file created");
 	}
 
 }
